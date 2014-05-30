3157ea3db50d3b027d0e667cf7784294592fad04
Andrea Shcherbakov <a.shcherbakov@ritoll.it>
2014-05-30 08:40:54 +0200

    born

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'codifica',
+    'password' => '7uNxbe8GW822JdHd',
+    'charset' => 'utf8',
+];
